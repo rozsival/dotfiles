828d7d083266af51927bde990b5d5eecb2259110
Vít Rozsíval <teekey99@gmail.com>
2023-09-20 20:37:05 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -24,7 +24,7 @@ brew install findutils
 brew install gnu-sed
 # Install a modern version of Bash.
 brew install bash
-brew install bash-completion2
+brew install bash-completion@2
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
